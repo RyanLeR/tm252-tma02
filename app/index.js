74de3a27f997330f0004a8526544f157feb83e75
Ryan LR <ryan.leroux77@outlook.com>
2026-06-14 21:49:31 +0100

    tma02 card bodies now showing

--- app/index.js
+++ app/index.js
@@ -1,2 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+    const tm252_tma01 = document.getElementById("tm252-25b-tma01-body");
+    const m269_tma03 = document.getElementById("m269-24j-tma03-body");
+    tm252_tma01.classList.remove("d-none");
+    tm252_tma01.classList.remove("d-block");
+
+    m269_tma03.classList.remove("d-none");
+    m269_tma03.classList.remove("d-block");
+
+    
 });
